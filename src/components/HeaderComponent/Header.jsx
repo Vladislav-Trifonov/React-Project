87f975fffd6bed66e00bb194e9194db0a1b60ca5
Vladislav-Trifonov <vladislav.trifonov57@gmail.com>
2023-11-13 17:05:36 +0200

    Starting the project

---
+++ src/components/HeaderComponent/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <header className="header"> 
+            <nav>
+                <ul>
+                    <li className="navItem">Начало</li>
+                    <li className="navItem">Продукти</li>
+                    <li className="navItem">За Нас</li>
+                    <li className="navItem">Контакти</li>
+                </ul>
+            </nav>
+
+            <img src="./logo.JPG" alt="" />
+        </header>
+    )
+}; 
+
+export default Header; 
