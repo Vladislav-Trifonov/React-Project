f05eebc81fcbff469708baed1dbc8105cdca26c4
Vladislav-Trifonov <vladislav.trifonov57@gmail.com>
2023-12-02 23:30:23 +0200

    Adding Font Awesome and making style changes

--- sj-products/src/components/HomeComponent/Home.jsx
+++ sj-products/src/components/HomeComponent/Home.jsx
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import "./home-component.scss";
 
-function Home ({ components }) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
+import React, { useState } from "react";
+
+function Home({ components }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextComponent = () => {
@@ -16,14 +21,16 @@ function Home ({ components }) {
   return (
     <div className="carousel">
       {components.map((Component, index) => (
-        <div key={index} style={{ display: index === currentIndex ? 'block' : 'none' }}>
+        <div key={index} className={index === currentIndex ? "visible" : "hidden"}>
           <Component />
         </div>
       ))}
-      <button onClick={prevComponent}>&lt;</button>
-      <button onClick={nextComponent}>&gt;</button>
+      <div className="carousel-btns">
+        <button onClick={prevComponent} className="change-button"><FontAwesomeIcon icon={faAngleLeft} className="font-awesome-icon"/></button>
+        <button onClick={nextComponent} className="change-button"><FontAwesomeIcon icon={faAngleRight} className="font-awesome-icon"/></button>
+      </div>
     </div>
   );
-};
+}
 
 export default Home;
